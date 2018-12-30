6102ae5576950276e772be1e5286d1a762a0c07c
Daniel Xiha <40915588+HazlerDaniel@users.noreply.github.com>
2018-12-30 18:52:42 +0700

    Add files via upload

---
+++ program menghapus data.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main (){
+	int A [5]={20,7,2001,200,13};
+	int n,hapus;
+	
+	cout<<"Data yang lama\n";
+	for (n=0;n<5;n++)
+	{ cout<< " "<<A[n];}
+	cout<<"data yang ingin di hapus : ";
+	cin>>hapus;
+	cout<<"\nData yang baru : \n";
+	for (n=hapus-1;n<5-1;n++)
+	{A[n]=A[n+1];}
+	for (n=0;n<4;n++)
+	{cout<< " "<<A[n];
+	}
+	getch();
+	return 0;
+	
+}
